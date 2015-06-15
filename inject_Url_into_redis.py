96e00e49051794ce928e95a2e9d7f94544ed88bb
yidun55 <heshang1203@sina.com>
2015-06-15 18:08:05 +0800

    redis work

---
+++ inject_Url_into_redis.py
@@ -0,0 +1,22 @@
+#!usr/bin/env python
+#encoding: utf-8
+
+import redis
+
+def injectUrl(key, values_list):
+    """
+    inject url into list of redis
+    """
+    assert type(values_list)==list, 'values_list\
+            must be list'
+    myRedis = redis.StrictRedis(host='localhost',\
+            port=6379)
+    for value in values_list:
+        myRedis.lpush(key, value)
+
+
+baseUrl = 'http://www.p2pzxw.com/index.asp?Page='
+urls = [baseUrl+str(i) for i in xrange(1,11)]
+key = 'p2p_zxw_redis_spider:start_urls'
+
+injectUrl(key, urls)
